f08d8bdb6144ca1a494a57bbda1b6b2b94b594a3
skatn <87296704+skatn@users.noreply.github.com>
2024-02-06 10:22:21 +0900

    Initial commit

---
+++ src/main/java/com/koreanbrains/onlinebutlerback/OnlineButlerBackApplication.java
@@ -0,0 +1,13 @@
+package com.koreanbrains.onlinebutlerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineButlerBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineButlerBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koreanbrains/onlinebutlerback/OnlineButlerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreanbrains.onlinebutlerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineButlerBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
