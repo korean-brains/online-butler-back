5bef8d7ec443d5e1d15e4e396681c1af85be0f75
Lee Minki <66666704+cyonits@users.noreply.github.com>
2024-02-14 17:21:00 +0900

    Update ErrorCode.java

--- src/main/java/com/koreanbrains/onlinebutlerback/common/exception/ErrorCode.java
+++ src/main/java/com/koreanbrains/onlinebutlerback/common/exception/ErrorCode.java
@@ -6,11 +6,17 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    FILE_NOT_UPLOADED(500, "S3_001", "파일을 업로드하지 못했습니다."),
+    //400
 
-    POST_NOT_FOUND(404, "P_001", "포스트를 찾을 수 없습니다."),
-
+    //403
     PERMISSION_DENIED(403, "PM_001", "권한이 없습니다."),
+
+    //404
+    POST_NOT_FOUND(404, "P_001", "포스트를 찾을 수 없습니다."),
+    MEMBER_NOT_FOUND(404, "M_001", "멤버를 찾을 수 없습니다."),
+
+    //500
+    FILE_NOT_UPLOADED(500, "S3_001", "파일을 업로드하지 못했습니다."),
 
     ;
     
