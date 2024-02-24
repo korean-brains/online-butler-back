50a18a608fe84df5b847cd7fc369cbe2c736f942
skatn <87296704+skatn@users.noreply.github.com>
2024-02-24 17:50:39 +0900

    feat: 답글 작성, 삭제 기능 추가
    #3

--- src/main/java/com/koreanbrains/onlinebutlerback/service/comment/CommentService.java
+++ src/main/java/com/koreanbrains/onlinebutlerback/service/comment/CommentService.java
@@ -4,6 +4,7 @@ import com.koreanbrains.onlinebutlerback.common.exception.EntityNotFoundExceptio
 import com.koreanbrains.onlinebutlerback.common.exception.ErrorCode;
 import com.koreanbrains.onlinebutlerback.common.exception.PermissionDeniedException;
 import com.koreanbrains.onlinebutlerback.entity.comment.Comment;
+import com.koreanbrains.onlinebutlerback.entity.comment.ReplyComment;
 import com.koreanbrains.onlinebutlerback.entity.member.Member;
 import com.koreanbrains.onlinebutlerback.entity.post.Post;
 import com.koreanbrains.onlinebutlerback.repository.comment.CommentRepository;
@@ -53,7 +54,39 @@ public class CommentService {
         commentRepository.delete(findComment);
     }
 
+    @Transactional
+    public Long writeReply(Long commentId, Long authorId, String text) {
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new EntityNotFoundException(ErrorCode.COMMENT_NOT_FOUND));
+        Member author = memberRepository.findById(authorId)
+                .orElseThrow(() -> new EntityNotFoundException(ErrorCode.MEMBER_NOT_FOUND));
+
+        ReplyComment reply = ReplyComment.builder()
+                .author(author)
+                .comment(comment)
+                .text(text)
+                .build();
+
+        return replyCommentRepository.save(reply).getId();
+    }
+
+    @Transactional
+    public void deleteReply(Long replyId, Long memberId) {
+        ReplyComment findReply = replyCommentRepository.findById(replyId)
+                .orElseThrow(() -> new EntityNotFoundException(ErrorCode.COMMENT_NOT_FOUND));
+
+        validateReplyPermission(memberId, findReply);
+
+        replyCommentRepository.delete(findReply);
+    }
+
     private void validateCommentPermission(Long memberId, Comment comment) {
+        if(!Objects.equals(comment.getAuthor().getId(), memberId)) {
+            throw new PermissionDeniedException(ErrorCode.PERMISSION_DENIED);
+        }
+    }
+
+    private void validateReplyPermission(Long memberId, ReplyComment comment) {
         if(!Objects.equals(comment.getAuthor().getId(), memberId)) {
             throw new PermissionDeniedException(ErrorCode.PERMISSION_DENIED);
         }
