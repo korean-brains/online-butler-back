7d28bf770737e5b87d0b598ae1e2df80a6c70026
skatn <87296704+skatn@users.noreply.github.com>
2024-03-04 19:46:10 +0900

    feat: 좋아요한 게시글 조회 기능 추가
    #4

---
+++ src/main/java/com/koreanbrains/onlinebutlerback/controller/post/LikePostScrollRequest.java
@@ -0,0 +1,4 @@
+package com.koreanbrains.onlinebutlerback.controller.post;
+
+public record LikePostScrollRequest(Long cursor, int size) {
+}
---
+++ src/main/java/com/koreanbrains/onlinebutlerback/repository/post/LikePostScrollDto.java
@@ -0,0 +1,4 @@
+package com.koreanbrains.onlinebutlerback.repository.post;
+
+public record LikePostScrollDto(Long id, String caption) {
+}
